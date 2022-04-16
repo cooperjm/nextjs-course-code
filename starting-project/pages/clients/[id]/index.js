ed2c501f268c429d5ff523e0135587705376623c
cooperjm <cooper.jm@outlook.com>
2022-04-16 15:30:42 -0400

    02 course work

--- starting-project/pages/clients/[id]/index.js
+++ starting-project/pages/clients/[id]/index.js
@@ -4,5 +4,22 @@ import { useRouter } from 'next/router';
 export default function ClientProjectsPage() {
 	const router = useRouter();
 	console.log(router.query);
-	return <h1>The Client Projects Page</h1>;
+
+	function loadProjectHandler() {
+		// load data...
+		router.push({
+			pathname: '/clients/[id]/[clientprojectid]',
+			query: {
+				id: 'max',
+				clientprojectid: 'projecta'
+			}
+		});
+	}
+
+	return (
+		<div>
+			<h1>The Projects of a Given Client</h1>
+			<button onClick={loadProjectHandler}>Load Project A</button>
+		</div>
+	);
 }
