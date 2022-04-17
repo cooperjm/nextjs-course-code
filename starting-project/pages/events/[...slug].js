1d9c1c6063e49bef4661ef30a2bf8838adad15bc
cooperjm <cooper.jm@outlook.com>
2022-04-16 21:29:52 -0400

    routing project course work

---
+++ starting-project/pages/events/[...slug].js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FilteredEvensPage() {
+	return (
+		<div>
+			<h1>Filtered Events</h1>
+		</div>
+	);
+}
---
+++ starting-project/pages/events/[eventId].js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EventDetailPage() {
+  return (
+    <div><h1>Event Detail</h1></div>
+  )
+}
